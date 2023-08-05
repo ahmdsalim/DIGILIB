cbb2a5d15f674dd3a2955d98dd9df81caa21d144
Ahmad Salim <salimahmad14823@gmail.com>
2023-08-05 14:50:34 +0700

    Add migrations, models, controllers, helpers

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('uuid',36);
+            $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->nullableMorphs('userable');
+            $table->enum('role',['owner','sekolah','siswa','guru']);
+            $table->boolean('active')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            // $table->string('npsn')->nullable();
+            // $table->string('nisn')->nullable();
+            // $table->string('nip')->nullable();
+            
+            // $table->foreign('npsn')->references('sekolahs')->on('npsn')->onDelete('cascade')->onUpdate('cascade');
+            // $table->foreign('nisn')->references('siswas')->on('sisn')->onDelete('cascade')->onUpdate('cascade');
+            // $table->foreign('nip')->references('gurus')->on('nip')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
@@ -27,6 +38,11 @@ return new class extends Migration
      */
     public function down(): void
     {
+        // Schema::table('users', function(Blueprint $table) {
+        //     $table->dropForeign('npsn');
+        //     $table->dropForeign('nisn');
+        //     $table->dropForeign('nip');
+        // });
         Schema::dropIfExists('users');
     }
 };
