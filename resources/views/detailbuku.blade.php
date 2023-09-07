89350dc759b3870f2d7a993ec8b7d5775e9dc414
Viki Eka Pratama <vikimantep@gmail.com>
2023-09-07 13:21:38 +0700

    Add: tinggal rating

--- resources/views/detailbuku.blade.php
+++ resources/views/detailbuku.blade.php
@@ -29,10 +29,9 @@
                                 <div class="d-flex justify-content-center">
                                     @if ($buku->thumbnail)
                                         <img src="{{ asset('img/thumbnail-buku/' . $buku->thumbnail) }}"
-                                            class="thumbnail" style="width: 230px; height:300px;">
+                                            class="thumbnail">
                                     @else
-                                        <img src="{{ asset('img/default-pict.png') }}" class="thumbnail"
-                                            style="width: 230px; height:300px;">
+                                        <img src="{{ asset('img/default-pict.png') }}" class="thumbnail">
                                     @endif
 
                                 </div>
@@ -199,7 +198,11 @@
     border-radius: 0.4375rem;
     box-shadow: 0 0.125rem 0.25rem rgba(55,60,67,.075);
     max-width: 100%;
+    height: 250px;
+    width: 190px;
+    object-fit: cover;
     }
+
 </style>
 @endpush
 
